870047ea92288e8cb07a74b551a0a31f7658b063
A l - A l a m y <59588007+AL-AlamySploit@users.noreply.github.com>
2021-06-13 15:08:21 +0200

    Update hash.py

--- hash.py
+++ hash.py
@@ -44,13 +44,13 @@ if A1 == '1' :
     """)
     A1 = raw_input ("""Encrypt > """)
     if A1 == '1' :
-    encrypt = raw_input("Anything for Encryption: ")
+    encrypt = raw_input('Anything for Encryption: ')
     en=base64.b64encode(encrypt)
-    print ("+G [+] Your Encryption: " +en)
+    print ('+G [+] Your Encryption: ' +en)
   elif A1 == '2' :
-    decrypt = raw_input("Anything for Decryption: ")
+    decrypt = raw_input('Anything for Decryption: ')
     de = base64.b64decode(decrypt)
-    print ("+G [+] your Decryption: " +de)
+    print ('+G [+] your Decryption: ' +de)
 ######################################################
 elif A1 == '2' :
     import base64
@@ -60,13 +60,13 @@ elif A1 == '2' :
     """)
     A1 = raw_input ("""Encrypt > """)
     if A1 == '1' :
-    encrypt = raw_input("Anything for Encryption: ")
+    encrypt = raw_input('Anything for Encryption: ')
     et=base64.b32encode(encrypt)
-    print ("[+] Your Encryption: " +et)
+    print ('[+] Your Encryption: ' +et)
   elif A1 == '2' :
-    decrypt = raw_input("Anything for Decryption: ")
+    decrypt = raw_input('Anything for Decryption: ')
     dt = base64.b32decode(decrypt)
-    print ("[+] your Decryption: " +dt)
+    print ('[+] your Decryption: ' +dt)
 ######################################################
 elif A1 == '3' :
     import base64
@@ -76,48 +76,48 @@ elif A1 == '3' :
     """)
     A1 = raw_input ("""Encrypt > """)
     if A1 == '1' :
-    encrypt = raw_input("Anything for Encryption: ")
+    encrypt = raw_input('Anything for Encryption: ')
     ey=base64.b16encode(encrypt)
-    print ("[+] Your Encryption: " +ey)
+    print ('[+] Your Encryption: ' +ey)
   elif A1 == '2' :
-    decrypt = raw_input("Anything for Decryption: ")
+    decrypt = raw_input('Anything for Decryption: ')
     dy = base64.b16decode(decrypt)
-    print ("[+] your Decryption: " +dy)
+    print ('[+] your Decryption: ' +dy)
 ######################################################
 elif A1 == '4' :
-    in_user = raw_input("Enter anything for Encryption : ")
+    in_user = raw_input('Enter anything for Encryption : ')
     md4 = hashlib.md4(in_user).hexdigest()
-    print ("[+] Your Decryption : " + md4)
+    print ('[+] Your Decryption : ' + md4)
 ######################################################
 elif A1 == '5' :
-    in_user2 = raw_input("Enter anything for Encryption : ")
+    in_user2 = raw_input('Enter anything for Encryption : ')
     md5 = hashlib.md5(in_user2).hexdigest()
-    print ("[+] Your Decryption : " + md5)
+    print ('[+] Your Decryption : ' + md5)
 ######################################################    
 elif A1 == '6' :
-    in_user3= raw_input("Enter anything for Encryption : ")
+    in_user3= raw_input('Enter anything for Encryption : ')
     sha1 = hashlib.sha1(in_user3).hexdigest()
-    print ("[+] Your Decryption : " + sha1)
+    print ('[+] Your Decryption : ' + sha1)
 ##################################################
 elif A1 == '7' :
-    in_user4 = raw_input("Enter anything for Encryption : ")
+    in_user4 = raw_input('Enter anything for Encryption : ')
     sha224 = hashlib.sha224(in_user4).hexdigest()
-    print ("[+] Your Decryption : " + sha224)
+    print ('[+] Your Decryption : ' + sha224)
 ######################################################
 elif A1 == '8' : 
-    in_user5 = raw_input("Enter anything for Encryption : ")
+    in_user5 = raw_input('Enter anything for Encryption : ')
     sha256 = hashlib.sha256(in_user5).hexdigest()
-    print ("[+] Your Decryption : " + sha256)
+    print ('[+] Your Decryption : ' + sha256)
 ######################################################   
 elif A1 == '9' : 
-    in_user6 = raw_input("Enter anything for Encryption : ")
+    in_user6 = raw_input('Enter anything for Encryption : ')
     sha384 = hashlib.sha384(in_user6).hexdigest()
-    print ("[+] Your Decryption : " + sha384)
+    print ('[+] Your Decryption : ' + sha384)
 ######################################################
 elif A1 == '10' : 
-    in_user7 = raw_input("Enter anything for Encryption : ")
+    in_user7 = raw_input('Enter anything for Encryption : ')
     sha512 = hashlib.sha512(in_user7).hexdigest()
-    print ("[+] Your Decryption : " + sha512)
+    print ('[+] Your Decryption : ' + sha512)
 ######################################################
 elif A1 == '0' :
     sys.exit()
